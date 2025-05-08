62d366fc77b39af58b989c5616910a0cefef277f
Venkatesh T <venkatesht1243@gmail.com>
2025-05-08 19:34:06 +0530

    Add Axios interceptor to send token with all requests

--- src/config/api.js
+++ src/config/api.js
@@ -9,4 +9,16 @@ const api = axios.create({
   },
 });
 
+// Add a request interceptor to include the token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api; 
